a0261d8e2a6507ef267e2f23f13531517d432f29
Cristhian <jobs.cluna@gmail.com>
2022-12-02 06:22:05 -0500

    feat: add Icons comp

--- src/components/Icons/Icons.jsx
+++ src/components/Icons/Icons.jsx
@@ -4,7 +4,29 @@ import * as styles from'./icons.module.css'
 function Icons() {
   return (
     <div className={styles.icons}>
-      Icons
+      <div className={styles.iconsHolder}>
+          <div className={styles.icon}>
+          
+
+          </div>
+          <div className={styles.icon}>
+          
+          </div>
+          <div className={styles.icon}>
+          
+          </div>
+          <div className={styles.icon}>
+          
+          </div>
+          <div className={styles.icon}>
+
+          </div>
+      </div>
+      <div className={styles.community}>
+        <h1>Community</h1>
+        <p className={styles.comSubtitle}>Lorem ipsum dolor sit amet</p>
+        <p className={styles.comParagraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
     </div>
   )
 }
