c2ea5ef2b9936d4f1c059f37d0f62932569f6797
Cristhian <jobs.cluna@gmail.com>
2022-12-02 13:07:56 -0500

    feat: add Dummy Data

---
+++ src/utils/dummyData.js
@@ -0,0 +1,60 @@
+
+const data = [
+    {
+        title: "Incubator/Accelerator",
+        p1:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+        p2: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        icon: "incubator",
+        subtitle: "Popular Features",
+        feats:[
+            "Lorem ipsum dolor sit", "Lorem ipsum dolor sit", "Lorem ipsum dolor sit"
+        ],
+        button : "Get Started"
+    },
+    {
+        title: "Corporation",
+        p1:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+        p2: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        icon: "corporation",
+        subtitle: "Popular Features",
+        feats:[
+            "Lorem ipsum dolor sit", "Lorem ipsum dolor sit", "Lorem ipsum dolor sit"
+        ],
+        button : "Get Started"
+    },
+    {
+        title: "Investor",
+        p1:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+        p2: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        icon: "investor",
+        subtitle: "Popular Features",
+        feats:[
+            "Lorem ipsum dolor sit", "Lorem ipsum dolor sit", "Lorem ipsum dolor sit"
+        ],
+        button : "Get Started"
+    },
+    {
+        title: "Startup",
+        p1:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+        p2: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        icon: "startup",
+        subtitle: "Popular Features",
+        feats:[
+            "Lorem ipsum dolor sit", "Lorem ipsum dolor sit", "Lorem ipsum dolor sit"
+        ],
+        button : "Get Started"
+    },
+    {
+        title: "Open Challenge",
+        p1:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+        p2: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        icon: "openChallenge",
+        subtitle: "Popular Features",
+        feats:[
+            "Lorem ipsum dolor sit", "Lorem ipsum dolor sit", "Lorem ipsum dolor sit"
+        ],
+        button : "Get Started"
+    }
+]
+
+export default data
