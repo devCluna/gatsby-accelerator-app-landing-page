3b6d0b49421486fb835467b88571a3c054a5a94b
Cristhian <jobs.cluna@gmail.com>
2022-12-02 07:51:14 -0500

    Feat: add customer section comp

---
+++ src/components/Customers/Customers.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as styles from './customers.module.css'
+
+const Customers = () => {
+  return (
+    <div className={styles.customers}>
+      
+    </div>
+  )
+}
+
+export default Customers
